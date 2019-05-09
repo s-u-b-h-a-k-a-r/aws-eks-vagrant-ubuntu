1216ab03f87153cbc7c81574ccc139f2727a356a
SubhakarKotta <subhakarkotta@gmail.com>
2019-05-10 04:32:16 +0530

    aws cli is not installed on vm

--- provisioning/awscli/install.sh
+++ provisioning/awscli/install.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
  
 figlet pip
-curl -O https://bootstrap.pypa.io/get-pip.py
-python  get-pip.py --user
-export PATH=~/.local/bin:$PATH
-
-figlet pip version
-pip --version
-
-figlet aws-cli
-pip install awscli --upgrade --user
+apt-get install awscli -y
+apt-get install python3-pip -y
+pip3 install awscli --upgrade --user
 aws --version
 echo 'export PATH=~/.local/bin:$PATH' >> ~/.bashrc
