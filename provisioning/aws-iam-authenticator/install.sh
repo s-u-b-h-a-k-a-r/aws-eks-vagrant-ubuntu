c0a034303a98f9c0e4fd390797ead0c2355e9007
nekhilkotha <nekhil.guptha@gmail.com>
2019-06-05 13:50:49 +0530

    Providing ability to specify versions for kubectl iam-authenticator and terraform. Also changing the way to install docker

--- provisioning/aws-iam-authenticator/install.sh
+++ provisioning/aws-iam-authenticator/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
 figlet authenticator
-curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.12.7/2019-03-27/bin/linux/amd64/aws-iam-authenticator
+curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/$1/2019-03-27/bin/linux/amd64/aws-iam-authenticator
 chmod +x ./aws-iam-authenticator
 cp ./aws-iam-authenticator /home/vagrant/bin/aws-iam-authenticator && export PATH=/home/vagrant/bin:$PATH
 echo 'export PATH=/home/vagrant/bin:$PATH' >> ~/.bashrc
--- provisioning/kubectl/install.sh
+++ provisioning/kubectl/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 figlet kubectl
-curl -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.12.7/2019-03-27/bin/linux/amd64/kubectl
+curl -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/$1/2019-03-27/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir  -p /home/vagrant/bin && cp ./kubectl /home/vagrant/bin/kubectl && export PATH=/home/vagrant/bin:$PATH
 echo 'export PATH=/home/vagrant/bin:$PATH' >> ~/.bashrc
