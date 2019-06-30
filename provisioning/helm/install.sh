f3faceff9988876564cb1d261734abdb276c412f
SubhakarKotta <subhakarkotta@gmail.com>
2019-06-30 20:08:25 +0530

    HELM Version parameterized

--- provisioning/helm/install.sh
+++ provisioning/helm/install.sh
@@ -5,7 +5,7 @@ figlet HELM
 export PATH=/usr/local/bin:/home/vagrant/bin:$PATH
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
 chmod +x get_helm.sh
-./get_helm.sh
+./get_helm.sh -v v$1
 
 cp  /usr/local/bin/helm /home/vagrant/bin/helm
 cp  /usr/local/bin/tiller /home/vagrant/bin/tiller
